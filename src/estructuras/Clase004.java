f2ed3c5a6db673ec40bdab8b5ba8989f64c5a869
Andres Chuquiray <andreseduardo16398@gmail.com>
2020-06-30 16:38:00 -0500

    Clase001 - Clase004 Listo

---
+++ src/estructuras/Clase004.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuras;
+
+/**
+ *
+ * @author sandr
+ */
+public class Clase004 {
+    public static void main(String[] args) {
+        int num=1;
+        String mensaje = " Cristo viene pronto, preparate";
+        while (num<=10){
+            System.out.println(mensaje);
+            num++;
+        }
+    }
+    
+}
